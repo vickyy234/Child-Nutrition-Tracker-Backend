26181552c70b7cffb0495b69e6cbde191169ceb9
vickyy234 <vickymayilsamy@gmail.com>
2025-04-04 14:44:29 +0530

    Updated

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.listen(3000, () => {
+    console.log("Server started")
+})
+
+mongoose.connect('mongodb://localhost:27017/child-nutrition-tracker')
+    .then(() => {
+        console.log('DB connected!')
+    })
+    .catch((err) => {
+        console.log('DB not connected ', err)
+    })
