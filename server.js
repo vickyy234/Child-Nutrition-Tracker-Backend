9980429cbb3db53e6b3067634d4deb0728e92b9d
vickyy234 <vickymayilsamy@gmail.com>
2025-04-04 15:18:23 +0530

    Updated

--- server.js
+++ server.js
@@ -8,14 +8,35 @@ app.use(cors())
 app.use(bodyParser.json())
 require('dotenv').config()
 
-app.listen(process.env.PORT || 5000 , () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server started")
 })
 
-mongoose.connect('mongodb://localhost:27017/child-nutrition-tracker')
+mongoose.connect(process.env.MONGO_URl)
     .then(() => {
         console.log('DB connected!')
     })
     .catch((err) => {
         console.log('DB not connected ', err)
     })
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    number: Number,
+    password: String,
+    createdAt: { type: Date, default: Date.now }
+})
+
+const userModel = mongoose.model("userdata" , userSchema)  
+
+app.post('/', async (req, res) => {
+    try {
+        const { name, number, password } = req.body;
+        const res = new userModel({ name, number, password })
+        await res.save();
+        res.status(201).json({Message : 'Account Created' , name , number , password})
+    }
+    catch (err) {
+        res.status(500).json({Message : 'Account not created'})
+    }
+})
