d37693a492ae1f1e379f8a8eb4ebaccdef662a7d
vickyy234 <vickymayilsamy@gmail.com>
2025-04-04 14:52:02 +0530

    Updated

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(bodyParser.json())
+require('dotenv').config()
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000 , () => {
     console.log("Server started")
 })
 
