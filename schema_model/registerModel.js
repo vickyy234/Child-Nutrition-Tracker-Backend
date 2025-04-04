af5062f61df3fa9c63a0c39dff507976096f91e8
vickyy234 <vickymayilsamy@gmail.com>
2025-04-05 00:11:13 +0530

    Updated

---
+++ schema_model/registerModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const registerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    number: { type: Number, required: true },
+    password: { type: String, required: true },
+    state: { type: String, required: true },
+    district: { type: String, required: true },
+    taluk: { type: String, required: true },
+    registeredAt: { type: Date, default: Date.now, required: true }
+})
+const registerModel = mongoose.model("userRegisterdata", registerSchema)
+
+module.exports = registerModel
