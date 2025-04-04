aa91b2824c736f9635ef7003fb31134dc7666ec9
vickyy234 <vickymayilsamy@gmail.com>
2025-04-05 00:24:54 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 
 //Middleware
 app.use(cors())
-app.use(bodyParser.json())
 app.use(express.json())
 require('dotenv').config()
 
